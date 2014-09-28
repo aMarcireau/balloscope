6ec9c3df0f325538e9d1f8ef14596a0da04d83cd
Zboubidoo <alexandre.marcireau@student.ecp.fr>
2014-09-28 16:12:28 +0200

    Add FOS jsrouting Bundle
    Run « php composer.phar update »

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             
             new Balloscope\CoreBundle\BalloscopeCoreBundle(),
         );
